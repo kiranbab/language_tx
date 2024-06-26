4dcc4d0124d55f37fd77ed1b9390862059a6f9b3
kiranbab <kiran2babu@gmail.com>
2024-06-27 00:23:15 +0530

    attention

--- model.py
+++ model.py
@@ -59,7 +59,6 @@ class LayerNormalization(nn.Module):
     
 class FeedForwardBlock(nn.Module):
     
-    
     def __init__(self,d_model:int,d_ff:int,dropout:float) -> None:
         super().__init__()
         self.linear_1 = nn.Linear(d_model,d_ff)# w1 and b1 
@@ -70,29 +69,54 @@ class FeedForwardBlock(nn.Module):
         # (batch,seq_len,d_model ) -->(batch,seq_len,d_ff) --> (batch,seq_len,d_model)
         return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
     
-    class MultiHeadAttentionBlock(nn.Module):
+class MultiHeadAttentionBlock(nn.Module):
+    
+    def __init__(self,d_model:int,h:int,dropout:float) -> None:
         
-        def __init__(self,d_model:int,h:int,dropout:float) -> None:
-            
-            super().__init__()
-            self.d_model = d_model
-            self.h = h 
-            assert d_model% h ==0 , "d_model is not divisible by h"
-            
-            self.d_k = d_model//h 
-            self.w_q = nn.Linear(d_model,d_model) #Wq 
-            self.w_k = nn.Linear(d_model,d_model) # Wk 
-            self.w_v = nn.Linear (d_model,d_model) #Wv 
-            
-            self.w_o = nn.Linear(d_model,d_model) # Wo 
-            self.dropout = nn.Dropout(dropout)
+        super().__init__()
+        self.d_model = d_model
+        self.h = h 
+        assert d_model% h ==0 , "d_model is not divisible by h"
+        
+        self.d_k = d_model//h 
+        self.w_q = nn.Linear(d_model,d_model) #Wq 
+        self.w_k = nn.Linear(d_model,d_model) # Wk 
+        self.w_v = nn.Linear (d_model,d_model) #Wv 
+        
+        self.w_o = nn.Linear(d_model,d_model) # Wo 
+        self.dropout = nn.Dropout(dropout)
 
-        def forward(self,q,k,v,mask):
+    @staticmethod 
+    def attention(query,key,value,mask,droput:nn.Dropout):
+        d_k = query.shape[-1]
 
-            query = self.w_q(q) # (batch,seq_len,d_model0 -->(batch,seq_len,d_model)
-            key = self.w_k(k)# (batch,seq_len,d_model0 -->(batch,seq_len,d_model)
-            value = self.w_v(v)# (batch,seq_len,d_model0 -->(batch,seq_len,d_model) 
+        #(batch_size,seq_len,d_k) -> (batch_size,seq_len,seq_len)
+        attention_score = (query @ key.tranpose(-2,-1) ) / math.sqrt(d_k)
 
+        if mask is not None:
+            attention_score.masked_fill(mask==0,-1e9)
+        attention_score = attention_score.softmax(dim=-1) #(batch,seq_len,seq_len)
+        if dropout is not None:
+            attention_score = dropout(attention_score)
+
+        return (attention_score@ value),attention_score
+
+
+
+    def forward(self,q,k,v,mask):
+
+        query = self.w_q(q) # (batch,seq_len,d_model0 -->(batch,seq_len,d_model)
+        key = self.w_k(k)# (batch,seq_len,d_model0 -->(batch,seq_len,d_model)
+        value = self.w_v(v)# (batch,seq_len,d_model0 -->(batch,seq_len,d_model) 
+
+
+        # (batch-size,seq_len,d_model) --> (batch-size,seq_len,h,d_k) -> (batch_size,sh,seq_len,d_k)
+        # we doing the transpose here becuase we want eveyr head to see the entire sequence of length but with diffent d_k 
+        query = query.view(query.shape[0],query.shape[1],self.h,self.d_k).tranpose(1,2)
+        key  = key.view(key.shape(0),key.shape[1],self.h,self.d_k).tranpose(1,2)
+        value = value.view(value.shape[0],value.shape[1]self.h,self.d_k).tranpose(1,2)
+
+        x,self.attention_score = MultiHeadAttentionBlock(query,key,value,mask,self.dropout)
 
 
 
